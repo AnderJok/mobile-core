22e54031dc717f2bc6463ae8b8fc86ef57314b7e
AnderJok <ander.aranguren.c@gmail.com>
2026-06-07 16:23:17 +0200

    Fix unused import

--- src/app/features/workout/pages/workouts/workouts.ts
+++ src/app/features/workout/pages/workouts/workouts.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { WorkoutFilter } from '../../components/workout-filter/workout-filter';
-import { WorkoutBuilder } from '../workout-builder/workout-builder';
 import { WorkoutList } from '../../components/workout-list/workout-list';
 
 @Component({
   selector: 'app-workouts',
-  imports: [WorkoutList, WorkoutBuilder, WorkoutFilter],
+  imports: [WorkoutList, WorkoutFilter],
   templateUrl: './workouts.html',
   styleUrl: './workouts.css',
 })
