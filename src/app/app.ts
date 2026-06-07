a724b28e6d74af212402a5f7f0d37ee5e723d8c6
AnderJok <ander.aranguren.c@gmail.com>
2026-06-07 17:39:24 +0200

    Add some css structure for the main page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './core/components/navbar/navbar';
+import { GlobalPlayer } from './core/components/global-player/global-player';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, GlobalPlayer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
