e1874dc483125c740d1f29a0ec7153a72356593b
AnderJok <ander.aranguren.c@gmail.com>
2026-06-09 22:43:08 +0200

    Fix anchors and buttons for a proper navigation

--- src/app/core/components/global-player/global-player.ts
+++ src/app/core/components/global-player/global-player.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { WorkoutPlayerService } from '../../services/workout-player.service';
 
 @Component({
@@ -9,4 +10,17 @@ import { WorkoutPlayerService } from '../../services/workout-player.service';
 })
 export class GlobalPlayer {
   workoutPlayerService = inject(WorkoutPlayerService);
+  private readonly router = inject(Router);
+
+  goToWorkout() {
+    const activeWorkout = this.workoutPlayerService.activeWorkout();
+    if (activeWorkout) {
+      this.router.navigate(['/active-workout', activeWorkout.id]);
+    }
+  }
+
+  stopWorkout() {
+    this.workoutPlayerService.stopWorkout();
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/features/workout/components/workout-list/workout-list.ts
+++ src/app/features/workout/components/workout-list/workout-list.ts
@@ -1,25 +1,20 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { WorkoutPlayerService } from '../../../../core/services/workout-player.service';
 import { MockService } from '../../../../core/services/mock.service';
+import { Workout } from '../../../../shared/interfaces/workout';
 
 @Component({
   selector: 'app-workout-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './workout-list.html',
   styleUrl: './workout-list.css',
 })
 export class WorkoutList {
   workoutPlayerService = inject(WorkoutPlayerService);
   readonly mockService = inject(MockService);
-  startWorkout() {
-    const workout = {
-      id: '1',
-      name: 'Test Workout',
-      description: 'This is a test workout',
-      tags: ['test', 'workout'],
-      blocks: []
-    };
+
+  startWorkout(workout: Workout) {
     this.workoutPlayerService.playWorkout(workout);
-    // go to active workout page.
   }
 }
