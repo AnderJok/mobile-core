f4e9aa0e4add743018aea3305c047d266d7f0417
AnderJok <ander.aranguren.c@gmail.com>
2026-06-20 12:00:37 +0200

    Change workout-builder button for an anchor and add navigation to workout-builder page

--- src/app/features/workout/pages/workouts/workouts.ts
+++ src/app/features/workout/pages/workouts/workouts.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WorkoutFilter } from '../../components/workout-filter/workout-filter';
 import { WorkoutList } from '../../components/workout-list/workout-list';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-workouts',
-  imports: [WorkoutList, WorkoutFilter],
+  imports: [WorkoutList, WorkoutFilter, RouterLink],
   templateUrl: './workouts.html',
   styleUrl: './workouts.css',
 })
